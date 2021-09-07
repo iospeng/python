0fd6164a16db98be9951868320e2a4407c1ec025
iospeng <iospeng@163.com>
2021-09-07 15:35:36 +0800

    等待方式及定位方式

---
+++ pycharm_demo/location_demo/location/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Project : location_demo
+# @File    : __init__.py.py
+# @Software: PyCharm
+# @Author  : Lizhipeng
+# @Email   : 1907878011@qq.com
+# @Time    : 2021/9/7 13:16
